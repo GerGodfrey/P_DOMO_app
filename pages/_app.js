f6857f7bdc1d8b1055e01a30906a16d354f6297c
GerGodfrey <luis-godfrey@hotmail.com>
2023-03-17 14:30:25 -0600

    modificaciones a paginas principales

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import Link from 'next/link'
+import Navbar from '../components/Navbar'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </div> 
+  )
 }
+
+
+export default App
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from '../components/Navbar'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        
+        {/* <Main /> */}
         <NextScript />
       </body>
     </Html>
