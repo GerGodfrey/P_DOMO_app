37ea86fd36f5008e241761c655349bb6a3de1c32
GerGodfrey <luis-godfrey@hotmail.com>
2023-03-17 17:21:18 -0600

    navbar en pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import Link from 'next/link'
-import Navbar from '../components/Navbar'
 
 function App({ Component, pageProps }) {
   return (
     <div>
-      <Navbar></Navbar>
       <Component {...pageProps} />
     </div> 
   )
