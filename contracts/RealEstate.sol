a19a656d506b316d5e1fe5b957533187a58e5e17
GerGodfrey <luis-godfrey@hotmail.com>
2023-03-20 12:25:22 -0600

    integracion de factory

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -9,20 +9,25 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract RealEstate is ERC721URIStorage {
     
-    uint256 public maxSupply = 2;
+    uint256 public maxSupply;
+    //para que era ste ?
     address contractAddress;
+    address creator;
+    string tokenDATA;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    
-    constructor(  ) ERC721("Real Estate", "REAL"){ //TODO : address marketplaceAddress 
-        // contractAddress = marketplaceAddress;
+    //TODO : address marketplaceAddress
+    constructor(uint256 _maxSupply, string memory _tokenDATA, address _creator ) ERC721("Real Estate", "REAL"){  
+        maxSupply = _maxSupply;
+        tokenDATA = _tokenDATA;
+        creator = _creator;
     }
 
 
     // Mintear n veces la misma casa
-    function mint(string memory tokenURI) public returns (uint256) {
+    function mint() public returns (uint256) {
         //confirmamos requerimientos 
         require(_tokenIds.current() < maxSupply, "I'm sorry we reached the cap");
 
@@ -31,13 +36,14 @@ contract RealEstate is ERC721URIStorage {
         uint256 newItemId = _tokenIds.current();
 
         // mintear el nuevo ID y mandárselo al sender 
+        // msg.sender = buyer 
         _mint(msg.sender, newItemId);
         
         // cambiar el TokenURI de un ID 
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, tokenDATA);
 
-        //Dar permisos al contractAddress para que maneje todo 
-        setApprovalForAll(contractAddress, true);
+        //Dar permisos al contractAddress(marketPlace) para que maneje todo
+        //setApprovalForAll(contractAddress, true);
 
         return newItemId;
     }
@@ -46,4 +52,20 @@ contract RealEstate is ERC721URIStorage {
         return _tokenIds.current();
     }
 
+}
+
+
+contract Factory {
+   RealEstate[] public RealEstateArray;
+   
+   function CreateNewRealEstate(uint256 _maxSupply, string memory _tokenURI) public {
+    //console.log("Creator of New Real Estate: ",msg.sender);
+    RealEstate realestate = new RealEstate(_maxSupply, _tokenURI, msg.sender);
+    RealEstateArray.push(realestate);
+   }
+
+   function totalRealEstate() public view returns (uint256){
+        uint256 res = RealEstateArray.length ; 
+        return res;
+   }
 }
