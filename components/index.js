d90e3428a57abb421144433813ad446e3dab7e40
GerGodfrey <luis-godfrey@hotmail.com>
2023-03-21 12:26:49 -0600

    PopHome implementacion

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import Navbar from './Navbar';
 import Search from './Search';
+import PopHome from './PopHome';
 
 export{
     Navbar,
-    Search
+    Search,
+    PopHome
 };
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
@@ -10,9 +11,13 @@ import { sc_factory_localhost, sc_realState_localhost, sc_scrow_localhost } from
 import Escrow from '../artifacts/contracts/Escrow.sol/Escrow.json'
 import Factory from '../artifacts/contracts/RealEstate.sol/Factory.json'
 import RealEstate from '../artifacts/contracts/RealEstate.sol/RealEstate.json'
-import {Navbar, Search} from '../components'
-
-
+import {Navbar, Search, PopHome} from '../components'
+import { Polybase } from '@polybase/client'
+import { ethPersonalSign } from '@polybase/eth'
+import * as eth from "@polybase/eth"
+const path = require('path');
+require('dotenv').config({ path: path.resolve('config.env'),});
+const NEXT_PUBLIC_NAME_ESPACE  = process.env.NEXT_PUBLIC_NAME_ESPACE;
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -20,17 +25,16 @@ export default function Home() {
   const [account, setAccount] = useState(null)
   const [provider, setProvider] = useState(null)
   const [homes, setHomes] = useState([])
+  const [home, setHome] = useState([])
+  const [toggle, setToggle] = useState(false)
+  const [escrow, setEscrow] = useState(null)
+  
 
   const loadBlockchainData = async () => {
-    // const provider = new ethers.providers.JsonRpcProvider()
-    // const realStateContract = new ethers.Contract(sc_realState,RealEstate.abi,provider)
-    // const escrowContract = new ethers.Contract(sc_scrow,Escrow.abi,provider)
-    // console.log(realStateContract)
-    // console.log(escrowContract)
 
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     setProvider(provider)
-    const network = await provider.getNetwork()
+    //const network = await provider.getNetwork()
     const factory = new ethers.Contract(sc_factory_localhost, Factory.abi, provider)
     const total_rs = Number(await factory.totalRealEstate())
     const homes = []
@@ -38,15 +42,31 @@ export default function Home() {
     for( var i = 0 ; i< total_rs; i++){
       const address_re = await factory.RealEstateArray(i);
       const realEstate = new ethers.Contract(address_re, RealEstate.abi, provider);
-      const totalSupply = Number(await realEstate.totalSupply())
-      const maxSupply = Number(await realEstate.maxSupply())
+      const totalSupply = Number(await realEstate.totalSupply());
+      const maxSupply = Number(await realEstate.maxSupply());
       const data = await realEstate.tokenDATA();
-      const response = await fetch(data)
-      var metadata = await response.json()
-      metadata["percentage"] = totalSupply * 100 / maxSupply
-      homes.push(metadata)
+      const response = await fetch(data);
+      var metadata = await response.json();
+      metadata["address_re"] = address_re;
+      metadata["percentage"] = totalSupply * 100 / maxSupply;
+      homes.push(metadata);
     }
     setHomes(homes)
+  }
+
+
+  async function connectDB(address_re){
+
+    let db = new Polybase({
+      defaultNamespace: NEXT_PUBLIC_NAME_ESPACE,
+    });
+
+    const collectionReference = db.collection("Contracts5");
+    const records = await collectionReference.where("real_estate_contract", "==", address_re).get();
+    const escrow_contract = records.data[0].data.escrow_contract
+
+    return escrow_contract;
+
   }
 
   async function connect() { 
@@ -60,6 +80,16 @@ export default function Home() {
     loadBlockchainData()
   }, [])
 
+  const togglePop = (home) => {
+
+    const escrow_contract = connectDB(home.address_re);
+    const escrow = new ethers.Contract(escrow_contract,Escrow.abi, provider)
+    setEscrow(escrow)
+
+    setHome(home)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
   return (
     <div>
       <Navbar account={account} setAccount={setAccount} / >
@@ -69,19 +99,13 @@ export default function Home() {
         <h1> Home </h1>
         <hr />
         <div className = 'cards'>
-
           {homes.map((home,index) => (
-            <div className='card' key={index}>
+            <div className='card' key={index} onClick={() => togglePop(home)}> 
               <div className='card__image'>
                 <img src={home.image} alt='Home'/>
               </div>
               <div className='card__info'>
-                <h4> {home.attributes[0].value} ETH </h4>
-                {/* <p>
-                  <strong> {home.attributes[2].value} </strong> bds \ 
-                  <strong> {home.attributes[3].value} </strong> ba \
-                  <strong> {home.attributes[4].value} </strong> sqft \
-                </p> */}
+                <h4> {home.name} </h4>
                 <p> {home.address}</p>
                 <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
                   <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${home.percentage}%`}}>
@@ -89,12 +113,15 @@ export default function Home() {
                   </div>
                 </div>             
               </div>
-              
             </div>
           ))}
-
         </div>
       </div>
+
+      {toggle && (
+        <PopHome home={home} provider={provider} account={account} escrow={escrow} 
+          togglePop={togglePop} />
+      )}
 
     </div>
   );
