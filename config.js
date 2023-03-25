a7b6844626f604a363abbe4aec94b35d073ea21d
GerGodfrey <luis-godfrey@hotmail.com>
2023-03-25 12:33:19 -0600

    reinstalacion de paquetes

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 
-export const sc_factory_localhost = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const sc_factory_localhost = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+
+export const sc_factory_tesnet = "0x36db9F8383a73bEE62e3e10D121a7dC6a4Ac5A54"
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PK_WALLET_PRINCIPAL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
+      allowUnlimitedContractSize: true
     },
     mumbai: {
       url: API_URL, 
-      accounts: [`0x${PRIVATE_KEY}`], 
+      accounts: [`0x${PK_WALLET_PRINCIPAL}`],
+      allowUnlimitedContractSize: true,
+      gas: 2100000,
+      gasPrice: 80000000000,
     },
   },
   solidity: "0.8.17",
