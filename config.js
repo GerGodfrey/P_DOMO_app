9af66bd713dfadf1b3377b24a8d2f7ff342734ff
GerGodfrey <luis-godfrey@hotmail.com>
2023-04-03 23:04:58 -0600

    otros

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 
-export const sc_factory_localhost = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
-
+export const sc_factory_localhost = "0xAA292E8611aDF267e563f334Ee42320aC96D0463"
 export const sc_factory_tesnet = "0x36db9F8383a73bEE62e3e10D121a7dC6a4Ac5A54"
