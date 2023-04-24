36926ae3fa0705aca033fcd2681b52009e1fbdcd
GerGodfrey <luis-godfrey@hotmail.com>
2023-04-24 15:15:41 -0600

    :tada: new bytecode attribute

--- constants/index.js
+++ constants/index.js
@@ -13,3 +13,93 @@ export const navLinks = [
     title: "Mercado Secundario",
   },
 ];
+
+
+export const inputInfo = [
+  {
+    id: 'id',
+    placeholder: 'ID',
+    name: 'ID',
+  },
+  {
+    id: 'name',
+    placeholder: 'name',
+    name: 'Name',
+  },
+  {
+    id: 'address',
+    placeholder: 'address',
+    name: 'Address',
+  },
+  {
+    id: 'description',
+    placeholder: 'Description',
+    name: 'Description',
+  },
+  {
+    id: 'principal_image',
+    placeholder: 'url',
+    name: 'Principal Image',
+  },
+  {
+    id: 'purchase_price',
+    placeholder: 'Purchase Price',
+    name: 'Purchase Price',
+  },
+  {
+    id: 'decimals',
+    placeholder: 'Decimals',
+    name: 'Decimals',
+  },
+  {
+    id: 'fractions',
+    placeholder: 'Fractions',
+    name: 'Fractions',
+  },
+  {
+    id: 'bed_rooms',
+    placeholder: 'Bed Rooms',
+    name: 'Bed Rooms',
+  },
+  {
+    id: 'img_bed_rooms',
+    placeholder: 'url',
+    name: 'Image Bed Rooms',
+  },
+  {
+    id: 'bathrooms',
+    placeholder: 'Number of Bathrooms',
+    name: 'Number of Bathrooms',
+  },
+  {
+    id: 'img_bathrooms',
+    placeholder: 'url',
+    name: 'Image Bathrooms',
+  },
+  {
+    id: 'square_feet',
+    placeholder: 'Square Feet',
+    name: 'Square Feet',
+  },
+  {
+    id: 'year_built',
+    placeholder: 'Year Built',
+    name: 'Year Built',
+  },
+  {
+    id: 'residence',
+    placeholder: 'Type of Residence',
+    name: 'Residence',
+  },
+  {
+    id: '4img',
+    placeholder: 'url',
+    name: 'Other Image',
+  }
+  ,
+  {
+    id: '5img',
+    placeholder: 'url',
+    name: 'Other Image',
+  }
+]
