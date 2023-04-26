1597f4f63037f0028c138905754be202f9fe790b
GerGodfrey <luis-godfrey@hotmail.com>
2023-04-26 17:32:22 -0600

    nuevas paginas para AA y create

--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,6 @@ import { ethers } from 'ethers';
 function App({ Component, pageProps }) {
   const [account, setAccount] = useState()
   const [toggle, setToggle] = useState(false);
-  // const [provider, setProvider] = useState(null)
-  
-  // const connectHandler = async () => {
-  //   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  //   setAccount(accounts[0]);
-  //   console.log("ACCOUNT_NAVBAR:", accounts)
-  //   loadBlockchainData()
-  // }
-
-  // const loadBlockchainData = async () => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum)
-  //   setProvider(provider)
-  //   console.log("PROVIDER_NAVBAR:", provider)
-  // }
-
-  // useEffect ( () => {
-  //   //connectHandler()
-  // }, [])
-
   return (
       <UserContext.Provider value={[account, setAccount]}>
         <Layout>
