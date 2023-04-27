655af4009ae2df0f7e225685a132b0ccf05315f7
GerGodfrey <luis-godfrey@hotmail.com>
2023-04-27 00:38:11 -0600

    comentarios homemenu

--- components/home/home-menu.jsx
+++ components/home/home-menu.jsx
@@ -1,33 +1,33 @@
 
 
-const HomeMenu = () => {
-    return (
-      <div className="grid-cols-5">
-        <div className="link" style={{ color: '#6851ff'}}>
-          <a href="https://app.domopro.xyz" target="_blank" rel="noreferrer">
-            App Principal
-          </a>
-        </div>
+// const HomeMenu = () => {
+//     return (
+//       <div className="grid-cols-5">
+//         <div className="link" style={{ color: '#6851ff'}}>
+//           <a href="https://app.domopro.xyz" target="_blank" rel="noreferrer">
+//             App Principal
+//           </a>
+//         </div>
 
-        <div className="w-px h-5" style={{ backgroundColor: '#DDDBE0'  }} />
+//         <div className="w-px h-5" style={{ backgroundColor: '#DDDBE0'  }} />
 
-        <div className="link" style={{ color: '#6851ff'  }}>
-          <a href="https://dashboard.magic.link/signup" target="_blank" rel="noreferrer">
-            Tutoriales
-          </a>
-        </div>
+//         <div className="link" style={{ color: '#6851ff'  }}>
+//           <a href="https://dashboard.magic.link/signup" target="_blank" rel="noreferrer">
+//             Tutoriales
+//           </a>
+//         </div>
 
-        <div className="w-px h-5" style={{ backgroundColor: '#DDDBE0'  }} />
+//         <div className="w-px h-5" style={{ backgroundColor: '#DDDBE0'  }} />
 
-        <div className="link" style={{ color: '#6851ff' }}>
-          <a href="https://discord.gg/magiclabs" target="_blank" rel="noreferrer">
-            Ayuda
-          </a>
-        </div>
+//         <div className="link" style={{ color: '#6851ff' }}>
+//           <a href="https://discord.gg/magiclabs" target="_blank" rel="noreferrer">
+//             Ayuda
+//           </a>
+//         </div>
 
-      </div>
-    );
-  };
+//       </div>
+//     );
+//   };
   
-  export default HomeMenu;
+//   export default HomeMenu;
   
