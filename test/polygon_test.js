b827c7ea3f7c4b3671c3f53ef52d21b11305f6e7
GerGodfrey <luis-godfrey@hotmail.com>
2023-04-27 13:24:52 -0600

    test comentado

--- test/polygon_test.js
+++ test/polygon_test.js
@@ -14,3 +14,23 @@
 // }
 
 // main();
+
+// const { Polybase } = require ('@polybase/client');
+
+// async function main(){
+//     const dataBase_sc = new Polybase({
+//         defaultNamespace: "pk/0x7fd09c2b6e44027ed2b6e478a5ff36e201317a6d4734e3ae4868827740ecf53265bff10a510904fc12fd98e277fb8af107f463425346ae359b19f25754bbf9fb/DOMO",
+//       });
+    
+//     const collectionReference = dataBase_sc.collection("Tesnet5");
+    
+//     const records = await collectionReference.get("escrow_contract");
+//     console.log(records)
+//     console.log(records.data[0].data.escrow_contract)
+// }
+
+
+// main();
+
+
+
